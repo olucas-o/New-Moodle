7d76a40789969d9d4d25eeaa712da171b26410f1
Lucas Cel <lucas.ornelas.dev@gmail.com>
2026-01-08 14:24:35 -0300

    feat: Add language file for Instituto Celebra theme

---
+++ theme/celebra/lang/en/theme_celebra.php
@@ -0,0 +1,12 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Instituto Celebra';
+$string['choosereadme'] = 'Tema personalizado do Instituto Celebra';
